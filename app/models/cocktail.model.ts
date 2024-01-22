1f6380ed32b1ef4c0afd2051e6ec4b6047b3d176
Veren Villegas <verenvillegas@gmail.com>
2024-01-22 19:29:03 +1300

    Added models

---
+++ app/models/cocktail.model.ts
@@ -0,0 +1,30 @@
+import OpenAI from "openai";
+require("dotenv").config();
+
+const oApiKey = process.env.OPEN_API_KEY;
+
+const openai = new OpenAI({apiKey: oApiKey});
+
+const Cocktail = function(Cocktail : any){
+  let name = Cocktail.name;
+  name = name;
+}
+
+Cocktail.createDesc = async (title: string)  => {
+  try{
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: "system", content: "You are a helpful assistant." },
+        {role: "user", content: `Generate a small product description for the following cocktail name: ${title}`}],
+        model: "gpt-3.5-turbo-1106",
+        //response_format: {type: "json_object"},
+      });
+    console.log(completion.choices[0].message.content);
+    return completion.choices[0].message.content;
+  }catch(error){
+    return "Error: " + error;
+  }
+}
+
+module.exports = Cocktail;
+
+
